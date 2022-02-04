37a6cd53779fe2319f8fc9bb9bb88135d1093eea
Yeison <yeisonacabrerab@gmail.com>
2022-02-04 10:39:24 -0500

    cambios

--- js/saludo.js
+++ js/saludo.js
@@ -5,3 +5,28 @@ var nombre="maryury";
 
 console.log("Bienvenido (a) sra " + nombre);
 
+
+
+
+
+
+
+let producto=30000;
+let total=0;
+let impuesto=.10;
+let domi=2000;
+let valortotal=0;
+
+total=(producto * impuesto + producto );
+ valortotal=(total+domi);
+
+
+console.log( "valor total = "+ valortotal);
+
+
+
+
+
+
+
+
